dbcae238d6f7da55e4ce62824aeffccfd0d5ce18
Felix Uellendall <feluelle@pm.me>
2020-05-20 11:46:21 +0200

    Fix Bin2Dec length check

--- src/appideas/1-Beginner/bin2dec.go
+++ src/appideas/1-Beginner/bin2dec.go
@@ -7,7 +7,7 @@ import (
 
 // Bin2Dec Converts binary to decimal
 func Bin2Dec(binaryNumber string, length int) (int64, error) {
-	if len(binaryNumber) >= length {
+	if len(binaryNumber) > length {
 		return -1, errors.New("Binary number exceeds allowed length")
 	}
 	decimalNumber, _ := strconv.ParseInt(binaryNumber, 2, 64)
